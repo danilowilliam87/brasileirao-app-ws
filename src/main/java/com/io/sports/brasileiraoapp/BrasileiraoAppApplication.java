a7c1859fea60cce7926a4961e9158013a0b3a37b
Danilo William dos Reis Socorro <danilowilliam87@gmail.com>
2022-08-31 16:19:39 -0300

    feat:commit inicial do projeto

---
+++ src/main/java/com/io/sports/brasileiraoapp/BrasileiraoAppApplication.java
@@ -0,0 +1,13 @@
+package com.io.sports.brasileiraoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/sports/brasileiraoapp/BrasileiraoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.sports.brasileiraoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
