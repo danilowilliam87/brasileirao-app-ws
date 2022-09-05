14488e8ae606fefe686b0e346cc3336188c56ff2
Danilo William dos Reis Socorro <danilowilliam87@gmail.com>
2022-09-05 17:11:17 -0300

    feat: teste de unidade busca todas competicoes na camada de servico OK

--- src/test/java/com/io/sports/brasileiraoapp/InfoPartidaServiceTest.java
+++ src/test/java/com/io/sports/brasileiraoapp/InfoPartidaServiceTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 public class InfoPartidaServiceTest {
 
@@ -18,5 +21,19 @@ public class InfoPartidaServiceTest {
         Competicao competicao = new Competicao();
         competicao = this.service.findCompeticaoById(1L);
         Assertions.assertEquals("CAMPEONATO BRASILEIRO DE FUTEBOL - SÉRIE A - 2022",competicao.getNome());
+    }
+
+    @Test
+    public void buscaCompeticao2(){
+        Competicao competicao = new Competicao();
+        competicao = this.service.findCompeticaoByNome("CAMPEONATO BRASILEIRO DE FUTEBOL - SÉRIE A - 2022");
+        Assertions.assertEquals(1L,competicao.getId());
+    }
+
+    @Test
+    public void buscaCompeticao3(){
+        List<Competicao> lista = new ArrayList<>();
+        lista = this.service.findAllCompeticao();
+        Assertions.assertEquals(1, lista.size());
     }
 }
